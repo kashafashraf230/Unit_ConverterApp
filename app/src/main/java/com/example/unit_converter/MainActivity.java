49fe08dd18eefd65522b5b2545e3acf17a2f793c
kashafashraf230 <kashafashraf412@gmail.com>
2023-04-15 20:53:08 +0500

    Final commit

--- app/src/main/java/com/example/unit_converter/MainActivity.java
+++ app/src/main/java/com/example/unit_converter/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.unit_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText userValue = findViewById(R.id.UserValue);
+        Button button = findViewById(R.id.resultButton);
+        TextView resultValue = findViewById(R.id.result);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                String kgs = userValue.getText().toString();
+                Double kilograms = Double.parseDouble(kgs);
+
+                double pounds = (Double) kilograms * 2.20462;
+                resultValue.setText("" + pounds);
+            }
+        });
     }
 }
